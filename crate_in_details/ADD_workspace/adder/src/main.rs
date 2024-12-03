3d63c2fb3ee3f5c826b0c5182d8474671822f775
Prashant <prashantco111@gmail.com>
2024-12-03 17:56:03 +0530

    Complete workspace

---
+++ crate_in_details/ADD_workspace/adder/src/main.rs
@@ -0,0 +1,17 @@
+// use add_one;
+
+fn main() {
+    let num = 10;
+
+    println!(
+        "Hello world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+
+    println!(
+        "Hello world! {} plus two is {}!",
+        num,
+        add_two::add_two(num)
+    );
+}
